be8af81947301fa2ff3d792ca97f86f65efab792
brunolimadev <bruno.lima.dev@outlook.com>
2020-02-06 16:52:42 -0300

    Commit do arquivo bruno.js

---
+++ bruno.js
@@ -0,0 +1,10 @@
+const grupos = {
+    nome: 'Bruno Lima',
+    passatempo: 'Estudar, jogar futsal e comer :)',
+    esporte: 'futsal e snowboarding',
+    membrosDoGrupo = [
+        {nome: 'Gustavo'},
+        {nome: 'Guilherme'},
+        {nome: 'Edson'}
+    ]
+}
