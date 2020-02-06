4f61ef91355886d66e00d59f1541169b6b4c1209
brunolimadev <bruno.lima.dev@outlook.com>
2020-02-06 17:04:44 -0300

    Arquivo final meuGrupo.js

---
+++ meuGrupo.js
@@ -0,0 +1,32 @@
+let gustavo =
+{
+	nome: "Gustavo NNóbrega",
+	passatempo: "Tomar Cerveja!",
+	esporte: "Velejar",
+	membrosDoGrupo: ["Gustavo"]
+}
+
+let bruno = {
+    nome: 'Bruno Lima',
+    passatempo: 'Estudar, jogar futsal e comer :)',
+    esporte: 'futsal e snowboarding',
+    membrosDoGrupo = [
+        {nome: 'Gustavo'},
+        {nome: 'Guilherme'},
+        {nome: 'Edson'}
+    ]
+}
+
+let edson = {
+    Nome = "Edson Menezes",
+    Passatempo = "Video games",
+    Esporte = "Handboll",
+    MembrosDoGrupo = ["Edson Menezes"]
+}
+
+let guilherme = {
+    nome: "Guilherme",
+    passatempo: "Comer",
+    Esporte: "Corrida",
+    membrosDoGrupo: ["Bruno","Gustavo","Edson"]
+}
